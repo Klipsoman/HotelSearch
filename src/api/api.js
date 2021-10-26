a9b8493cab0820a44cd03816078d367801267f74
Sneik <Sneik92@ya.ru>
2021-10-26 15:27:26 +0300

    set https

--- src/api/api.js
+++ src/api/api.js
@@ -8,6 +8,6 @@ export function fetchHotels(location = 'moscow', checkIn = getDates().checkIn, c
     if(checkIn > checkOut) checkOut = dayPlusOne(checkIn);
     checkIn = dateNormalize(checkIn);
     checkOut = dateNormalize(checkOut);
-    return fetch(`http://engine.hotellook.com/api/v2/cache.json?token=d90bb770ade731a175bb6383cd79e6cd&currency=rub&location=${location}&checkIn=${checkIn}&checkOut=${checkOut}&lang=ru&limit=8`)
+    return fetch(`https://engine.hotellook.com/api/v2/cache.json?token=d90bb770ade731a175bb6383cd79e6cd&currency=rub&location=${location}&checkIn=${checkIn}&checkOut=${checkOut}&lang=ru&limit=8`)
     .then(res=>res.json())
 }
